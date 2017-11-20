1fee45feeb64266799dc325c814ce10443ca0234
kevin_kim <kevkim1120@gmail.com>
2017-11-20 10:59:26 -0800

    fixed import issues. setup.py should work now

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import shutil
 import overlays
 
 setup(
-    name = "new_overlay",
+    name = "pynq_overlay_demo",
     version = overlays.__version__,
     url = 'https://github.com/kevkim11/pynq_overlay_demo',
     license = 'All rights reserved.',
     author = "Kevin Kim",
     author_email = "kevkim11@email.com",
-    packages = ['new_overlay'],
+    packages = ['overlays'],
     package_data = {
     '' : ['*.bit','*.tcl','*.py','*.so'],
     },
     install_requires = ['pynq',],
     dependency_links=['http://github.com/xilinx/PYNQ'],
-    description = "New custom overlay for PYNQ-Z1"
+    description = "New simple custom overlay for PYNQ-Z1"
 )
